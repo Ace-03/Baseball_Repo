0388a80600603be92f8b6476693789534c0769b7
6185jcaron <66760646+Ace-03@users.noreply.github.com>
2025-02-18 14:19:31 -0700

    created new repo

---
+++ Baseball_Project/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,65 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Nullable`1<UnityEngine.Vector3> FiringSolution::Calculate(UnityEngine.Vector3,UnityEngine.Vector3,System.Single,UnityEngine.Vector3)
+extern void FiringSolution_Calculate_m48958A8AD8803FD7C24FCF96095B8D624D78FDE6 (void);
+// 0x00000002 System.Nullable`1<System.Single> FiringSolution::GetTimeToTarget(UnityEngine.Vector3,UnityEngine.Vector3,System.Single,UnityEngine.Vector3)
+extern void FiringSolution_GetTimeToTarget_m57A679464540E80F233B58ECC7567C23B6DC8F05 (void);
+// 0x00000003 System.Void FiringSolution::.ctor()
+extern void FiringSolution__ctor_m9EE46E64C2A05C9E6060239CF4D0621AE2C0C857 (void);
+// 0x00000004 System.Void Frog::Start()
+extern void Frog_Start_m4D05BF3B455D277C4A097C02C9F60D1873299FBD (void);
+// 0x00000005 System.Void Frog::Update()
+extern void Frog_Update_m38C0ABE53C7DB257212DE251875DB876DD45E078 (void);
+// 0x00000006 System.Void Frog::.ctor()
+extern void Frog__ctor_mBE5ACA4E8A3CEFBAACF2A15CD7ADCEB4B9658EB7 (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	FiringSolution_Calculate_m48958A8AD8803FD7C24FCF96095B8D624D78FDE6,
+	FiringSolution_GetTimeToTarget_m57A679464540E80F233B58ECC7567C23B6DC8F05,
+	FiringSolution__ctor_m9EE46E64C2A05C9E6060239CF4D0621AE2C0C857,
+	Frog_Start_m4D05BF3B455D277C4A097C02C9F60D1873299FBD,
+	Frog_Update_m38C0ABE53C7DB257212DE251875DB876DD45E078,
+	Frog__ctor_mBE5ACA4E8A3CEFBAACF2A15CD7ADCEB4B9658EB7,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	123,
+	122,
+	982,
+	982,
+	982,
+	982,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
